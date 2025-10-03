514862a50d106e2efd03b02b65f3128fd5028abc
Roberto Espeso <roberto.espeso96@gmail.com>
2025-10-03 09:10:03 -0300

    feat(ui): update visual components according to new client design

---
+++ js/btn-back-to-top.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById("btn-back-to-top");
+btn.style.display = "none";
+const footerOffset = 300;
+window.addEventListener("scroll", () => {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const windowHeight = window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+
+    // Mostrar si se bajó más de 200px y no estamos en los últimos 500px
+    if (scrollTop > 200 && scrollTop + windowHeight < documentHeight - footerOffset) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+});
+
+// Volver arriba suave
+btn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
